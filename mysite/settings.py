6404f27c01d19e82a5afee4ba28a6a48e8325c61
ranjitha <relaprolu@unomaha.edu>
2018-01-28 18:22:53 -0600

    Assignment 1p1 app deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,5 +140,5 @@ STATICFILES_DIRS = (
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
